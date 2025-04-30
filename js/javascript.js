668040d705d834ec2819a4ac5dd1de4e5ce9473a
Axel Villa <118828431+axelvilla@users.noreply.github.com>
2025-04-29 22:06:55 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+
+//Variables del proyecto
 let grupoTarjetas = ["😎", "🍦", "🐸", "👽", "👾", "🤖", "👹", "🐌", "🙈", "🙉", "🐷", "🦍","😍","🍟"];
 let totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
@@ -9,20 +11,24 @@ let spanContador = document.getElementById("count");
 let intentos = 0;
 let spanIntentos = document.getElementById("intentos");
 
-
+// Funcion para barajar las tarjetas
 function barajaTarjetas() {
   return totalTarjetas.sort(() => 0.5 - Math.random());
 }
 
+//Funcion para repartir las tarjetas
 function reparteTarjetas() {
   let mesa = document.querySelector("#mesa");
   let tarjetasBarajadas = barajaTarjetas();
   mesa.innerHTML = "";
 
+  //Por cada tarjeta barajada se le da un emoji dentro de un elemento div, donde se le da una clase tarjeta.
   tarjetasBarajadas.forEach(function (emoji) {
     let tarjeta = document.createElement("div");
     tarjeta.classList.add("tarjeta");
 
+
+    //Por cada tarjeta descubierta se le da una clase tarjeta contenido.
     let contenido = document.createElement("div");
     contenido.classList.add("tarjeta__contenido");
     contenido.textContent = emoji;
@@ -33,6 +39,7 @@ function reparteTarjetas() {
   });
 }
 
+//La funcion descubrir le da una clase "descubierta" a cada carta que se da vuelta.
 function descubrir() {
   if (bloqueo) return;
 
@@ -44,9 +51,12 @@ function descubrir() {
     segunda = this;
     bloqueo = true;
 
+    //Variables para seleccionar los emojis de las tarjetas
     let emoji1 = primera.querySelector(".tarjeta__contenido").textContent;
     let emoji2 = segunda.querySelector(".tarjeta__contenido").textContent;
 
+
+    //Si el emoji 1 es igual al emoji 2 se agrega 1 numero mas al contador y a los intentos. Además se bloquea la acción de dar vuelta la tarjeta.
     if (emoji1 === emoji2) {
       primera = null;
       segunda = null;
@@ -56,6 +66,7 @@ function descubrir() {
       intentos++;
       spanIntentos.textContent = intentos;
     } else {
+      //Si el emoji 1 no es igual al emoji 2 no se agrega 1 numero mas al contador pero si a los intentos. Se quita la funcion de bloqueo en la tarjeta y se vuelven a dar vuelta luego de 1 segundo.
       setTimeout(() => {
         primera.classList.remove("descubierta");
         segunda.classList.remove("descubierta");
